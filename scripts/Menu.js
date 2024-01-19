2216dce65795bbbd2b05823981ed9b077cd47185
Alexander Sapountzis <alexander.sapountzis@gmail.com>
2024-01-19 15:38:29 -0500

    Use a network first IndexedDB implementation

--- scripts/Menu.js
+++ scripts/Menu.js
@@ -10,13 +10,26 @@ const Menu = {
         });
     },
     load: async () => {
-        // Cache First
+        // Network First
         const db = await Menu.openDB();
-        if ((await db.count('categories')) == 0) {
+        try {
+            // We try to fetch from the network
             const data = await API.fetchMenu();
+            Menu.data = data;
+            console.log('Data from the network');
+            // If succeded, also update the cached version
+            db.clear('categories');
             data.forEach((category) => db.add('categories', category));
+        } catch (e) {
+            // Network error, we go to the cache
+            if ((await db.count('categories')) > 0) {
+                Menu.data = await db.getAll('categories');
+                console.log('Data from the cache');
+            } else {
+                // No cached data is available :(
+                console.log('No data is available');
+            }
         }
-        Menu.data = await db.getAll('categories');
         Menu.render();
     },
     getProductById: async (id) => {
